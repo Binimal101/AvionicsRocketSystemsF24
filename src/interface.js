401da3788eb878f7559d71f09990ecd765b055d1
ryanrnjit <rjr@njit.edu>
2024-11-25 17:16:28 -0500

    Added update interface function

---
+++ src/interface.js
@@ -0,0 +1,6 @@
+function update(w, x, y, z) {
+    document.getElementById("input_data").setAttribute("w_in", w);
+    document.getElementById("input_data").setAttribute("x_in", x);
+    document.getElementById("input_data").setAttribute("y_in", y);
+    document.getElementById("input_data").setAttribute("z_in", z);
+}
